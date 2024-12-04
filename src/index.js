cf9c0d7b9e672315a4d0219be373d775ff4f9df8
Clement Raux <ilesquafr@gmail.com>
2024-12-04 13:45:40 +0100

    création store redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './main.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/store.js';
+import { Provider } from 'react-redux';
+
+// Création du Provider pour que l'App entière est accès au store redux.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 

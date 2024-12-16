8b500c5ee89e2f9f8b9d90ee70836e0750815bd8
Clement Raux <ilesquafr@gmail.com>
2024-12-16 14:21:05 +0100

    fix accès page user de manière sécurisé

--- src/components/SecureRoute.js
+++ src/components/SecureRoute.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const SecureRoute = ({route}) => {
+// Modification de {route} en {children} car React ne comprennait pas automatiquement qu'il faut encapsuler les enfants
+//pour sécuriser la route du profil.
+const SecureRoute = ({children}) => {
 
-    const userLog = useSelector((state) => state.userLog);
+    const userLog = useSelector((state) => state.login.userLog);
 
     if (!userLog) {
         return <Navigate to="/sign-in" />;
     }
-    return route;
+    return children;
 };
 
 export default SecureRoute;
